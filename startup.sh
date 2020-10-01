400f79f80d1efdc23015cf10ccde388e10df49e9
Daryl Monge <daryl.monge@actian.com>
2020-09-30 21:09:06 -0500

    Support docker-compose secrets injection

--- startup.sh
+++ startup.sh
@@ -2,5 +2,14 @@
 
 # Special test harnass startup.
 
+if [ -f /run/secrets/id_rsa_pub ]
+then
+    # A known authenticated user is being injected from a container secret
+    mkdir -p ~actian/.ssh
+    cat /run/secrets/id_rsa_pub >>~actian/.ssh/authorized_keys
+    chown -R actian ~actian/.ssh
+    chmod -R u=rw,go= ~actian/.ssh/authorized_keys
+fi
+
 /sbin/sshd
 /usr/local/bin/dockerctl
